5d91080bf7bc4398de58a58949ba32af26788381
Sil Westerveld <dev@silwesterveld.com>
2015-05-20 17:22:37 +0200

    Write initial code for player class and functions and add empty engine to page.

--- js/app.js
+++ js/app.js
@@ -24,11 +24,28 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // TODO: implement
+}
+
+Player.prototype.update = function() {
+    // TODO: implement
+}
+
+Player.prototype.render = function() {
+    // TODO: implement
+}
+
+Player.prototype.handleInput = function() {
+    // TODO: implement
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+var player = new Player();
 
 
 
